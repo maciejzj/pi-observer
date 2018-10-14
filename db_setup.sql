13ea6b28b674ad7557dd7cc29acdd0e4416d25cf
Maciej Ziaja <maciejzjg@gmail.com>
2018-10-14 10:09:24 +0200

    Install python sql module and setup db for logs

--- db_setup.sql
+++ db_setup.sql
@@ -3,7 +3,10 @@ DROP USER 'root'@'localhost';
 CREATE USER 'root'@'localhost' IDENTIFIED BY 'balloonSroot';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost'
 
-CREATE DATABASE balloonS
-USE balloonS
+CREATE DATABASE balloonS;
+USE balloonS;
 
 CREATE TABLE users(id int, login varchar(25), pass varchar(25));
+
+CREATE TABLE press_log(num int NOT NULL AUTO_INCREMENT, log_time timestamp, log_val real, unit varchar(6), PRIMARY KEY (num));
+);
