c79e5bb0554a93b538a4f9f97e9a2ae47e18ff00
Maciej Ziaja <maciejzjg@gmail.com>
2018-10-15 18:19:18 +0200

    Remove old way of location logging

--- location_logger.py
+++ location_logger.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
+import mysql.connector
 from gps import gps
 
 gps = gps()
+record = gps.get_decimal_degrees_record()
 
-with open("/home/pi/balloonS/sensor_logs/loc_log", mode = "a+") as loc_log:	
-	loc_log.write(gps.make_gps_log_entry() + "\n")
-	
+mydb = mysql.connector.connect(
+	host = "localhost",
+	user = "root",
+	passwd = "balloonSroot",
+	database = "balloonS"
+)
 
-
+mycursor = mydb.cursor()
+sql = "INSERT INTO loc_log(log_time, latitude, longitude, velocity, course) VALUES (%s, %s, %s, %s, %s)"
+val = (record['timestamp'], record['latitude'], record['longitude'], record['velocity'], record['course'])
+mycursor.execute(sql, val)
+mydb.commit()
