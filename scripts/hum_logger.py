3707f89b7799ec93949874f55c45783cb2da8f12
Maciej Ziaja <maciejzjg@gmail.com>
2019-08-02 20:03:49 +0200

    Update script to read from hum sensor

--- scripts/hum_logger.py
+++ scripts/hum_logger.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import am2320
 import mysql.connector
 import datetime as dt
@@ -8,20 +8,21 @@ hum_sensor = am2320.AM2320(1)
 _, humidity = hum_sensor.readSensor()
 
 # Write to log text file.
-with open("/home/pi/balloonS/sensor_logs/hum_log", mode = "a+") as hum_log:	
+with open("/var/log/pi_observer/hum_log", mode = "a+") as hum_log:	
 	time_stamp = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 	hum_log.write(time_stamp + " h=" + str(humidity) + "%\n")
 	
 mydb = mysql.connector.connect(
 	host = "localhost",
-	user = "root",
-	passwd = "balloonSroot",
-	database = "balloonS"
+	user = "pi_observer_root",
+	passwd = "piobserverroot",
+	database = "pi_observer_data_logs"
 )
 
 # Write to log database.
 mycursor = mydb.cursor()
-sql = "INSERT INTO hum_log(log_time, log_val, unit) VALUES (%s, %s, %s)"
+sql = "INSERT INTO hum_log(time, value, unit) VALUES (%s, %s, %s)"
 val = (time_stamp, str(humidity), "hPa")
 mycursor.execute(sql, val)
 mydb.commit()
+
