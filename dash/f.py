48dda718f96b01e5dead57df54d621a959853327
Maciej Ziaja <maciejzjg@gmail.com>
2020-08-21 22:20:31 +0200

    Draft login form

--- dash/f.py
+++ dash/f.py
@@ -8,10 +8,13 @@ Complete project details: https://randomnerdtutorials.com
 '''
 
 import RPi.GPIO as GPIO
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
+from config import Config
+from forms import LoginForm
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 GPIO.setmode(GPIO.BCM)
 
@@ -23,6 +26,16 @@ pins = {
 for pin in pins:
    GPIO.setup(pin, GPIO.OUT)
    GPIO.output(pin, GPIO.LOW)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('main'))
+    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route("/")
